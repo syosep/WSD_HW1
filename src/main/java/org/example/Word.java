0c4694e4987808e9b528e81f241ae1c9eaf63ef9
심요셉 <s.yosep@kakao.com>
2024-10-22 10:52:20 +0900

    add word item

---
+++ src/main/java/org/example/Word.java
@@ -0,0 +1,47 @@
+package org.example;
+
+public class Word {
+    private int id;
+    private String word;
+    private String meaning;
+    private int level;
+
+    public Word(int id, String word, String meaning, int level) {
+        this.id = id;
+        this.word = word;
+        this.meaning = meaning;
+        this.level = level;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public String getMeaning() {
+        return meaning;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public void setMeaning(String meaning) {
+        this.meaning = meaning;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+}
