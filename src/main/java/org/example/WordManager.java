15a5d66f83d6a9ec55244501730f824436bb8126
심요셉 <s.yosep@kakao.com>
2024-10-22 10:57:43 +0900

    add WordManager interface

--- src/main/java/org/example/WordManager.java
+++ src/main/java/org/example/WordManager.java
@@ -1,4 +1,15 @@
 package org.example;
 
 public interface WordManager {
+    void listWord();
+    void listWordByLevel(int level);
+    void searchWord(String keyword);
+    void addWord(String word, String meaning, int level);
+    void modifyWord(int id, String word, String meaning, int level);
+    void deleteWord(int id);
+    void saveFile(String fileName);
+    void loadFile(String fileName);
 }
+
+
+
